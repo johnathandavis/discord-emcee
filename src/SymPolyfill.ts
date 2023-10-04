a4d136a9080f81b77bdcc8fc0b0896a20df061f8
John Davis <john@johndavis.dev>
2023-10-04 13:28:53 +1300

    Updated badges.

--- src/SymPolyfill.ts
+++ src/SymPolyfill.ts
@@ -1,7 +1,6 @@
-
 if (!Symbol.dispose) {
-    (Symbol as any).dispose ??= Symbol("Symbol.dispose");
+  (Symbol as any).dispose ??= Symbol('Symbol.dispose');
 }
 if (!Symbol.asyncDispose) {
-    (Symbol as any).Symbol.asyncDispose ??= Symbol("Symbol.asyncDispose");
+  (Symbol as any).Symbol.asyncDispose ??= Symbol('Symbol.asyncDispose');
 }
