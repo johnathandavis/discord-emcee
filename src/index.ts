059b38d920c2bb338bbadef8cde135d370446226
John Davis <john@johndavis.dev>
2023-10-05 21:57:01 +1300

    Updated tooling.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,17 @@ import { EmceeManager } from './EmceeManager';
 import * as sb from './StateBuilder';
 import { ChatInputCommandInteraction } from 'discord.js';
 
+/**
+ * Prompt a Discord user information via a MessageComponent.
+ * @param schema the schema for the data you're trying
+ * to get from the user
+ * @param uiOptions rendering options
+ * @param validator a function to determine if the
+ * partial state is ready for submission
+ * (i.e. whether or not to enable the Submit button)
+ * @param interaction the Discord interaction object
+ * @returns a value staisfying the schema (as a Promise)
+ */
 async function prompt<T extends sb.MCSchema<sb.MCRawShape>>(
   schema: T,
   uiOptions: EmceeUserInterface,
